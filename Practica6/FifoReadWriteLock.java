8f43a1eef20521b36d2627427ad176d4991b21ea
MarkTwin25 <72567940+MarkTwin25@users.noreply.github.com>
2024-11-16 16:30:55 -0600

    Update FifoReadWriteLock.java

--- Practica6/FifoReadWriteLock.java
+++ Practica6/FifoReadWriteLock.java
@@ -10,13 +10,12 @@
  * Modified 3nov24 Gilde Valeria R.
  */
 
-
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantLock;
-import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * First-in First-out readers/writers lock.
@@ -30,9 +29,8 @@ public class FifoReadWriteLock implements ReadWriteLock {
   Condition condition;
   Lock readLock;    // readers apply here
   Lock writeLock;   // writers apply here
-  AtomicInteger readerCount;
-  AtomicInteger writerCount;
-
+  int writeAcquires;
+  int writeReleases;
   
   public FifoReadWriteLock() {
     readAcquires = readReleases = 0;
@@ -41,8 +39,8 @@ public class FifoReadWriteLock implements ReadWriteLock {
     condition = metaLock.newCondition();
     readLock  = new ReadLock();
     writeLock = new WriteLock();
-    readerCount = new AtomicInteger(0);
-    writerCount = new AtomicInteger(0);
+    writeAcquires = 0;
+    writeAcquires = 0;
   }
   
   public Lock readLock() {
@@ -53,8 +51,6 @@ public class FifoReadWriteLock implements ReadWriteLock {
     return writeLock;
   }
   private class ReadLock implements Lock {
-
-    // Si un lector esta en su seccion critica, ningun escritor puede entrar a su propia seccion critica
     public void lock() {
       metaLock.lock();
       try {
@@ -71,18 +67,14 @@ public class FifoReadWriteLock implements ReadWriteLock {
         }
         //System.out.println("Writer: " + writer);
         readAcquires++;
-        readerCount.incrementAndGet();
       } finally {
         metaLock.unlock();
       }
     }
-
-
     public void unlock() {
       metaLock.lock();
       try {
         readReleases++;
-        readerCount.decrementAndGet();
         //System.out.println("Unlock ReadAcquires: " + readAcquires + " ReadReleases: " + readReleases);
 
         if (readAcquires == readReleases)
@@ -108,31 +100,33 @@ public class FifoReadWriteLock implements ReadWriteLock {
     }
   }
   private class WriteLock implements Lock {
-    // Si un escritor esta en su seccion critica, ningun lector puede entrar a su propia seccion critica
     public void lock() {
-        metaLock.lock();
-        try {
-          while (writer || readAcquires > readReleases) {
+      metaLock.lock();
+      try {
+
+        while (readAcquires != readReleases)
+          try {
             condition.await();
-          }
-          writer = true;
-          writerCount.incrementAndGet();
-        } catch (InterruptedException e) {
-          Thread.currentThread().interrupt();
-        } finally {
-          metaLock.unlock();
-        }
+          } catch (InterruptedException e) {}
+        
+        //System.out.println("Writer: " + writer);
+        writeAcquires++;
+      } finally {
+        metaLock.unlock();
       }
+    }
     public void unlock() {
       
-        metaLock.lock();
-        try {
-          writer = false;
-            writerCount.decrementAndGet();
-          condition.signalAll();
-        } finally {
+      metaLock.lock();
+      try{
+          writeReleases++;
+          if (writeAcquires == writeReleases){
+              condition.signalAll();
+          }
+        
+      } finally{
           metaLock.unlock();
-        }
+      }
       //System.out.println("Writer: " + writer);
     }
     public void lockInterruptibly() throws InterruptedException {
